aba8ab567af18a5dcd6c17ebd00afccd1f2dc3b5
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-07-09 09:32:26 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,29 +2,24 @@
 const request = require('request');
 const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-async function getCharacterNames(characterURL) {
-  try {
-    const response = await request.get(characterURL);
-    const characterData = JSON.parse(response.body);
-    return characterData.name;
-  } catch (error) {
-    console.error(error);
-    return null; 
-  }
-}
-
-async function getMovieCharacters(movieID) {
-  try {
-    const response = await request.get(`${API_URL}/films/${movieID}`);
-    const movieData = JSON.parse(response.body);
-    const characterPromises = movieData.characters.map(getCharacterNames);
-    const characterNames = await Promise.all(characterPromises);
-    console.log(characterNames.join('\n'));
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 if (process.argv.length > 2) {
-  getMovieCharacters(process.argv[2]);
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
 }
