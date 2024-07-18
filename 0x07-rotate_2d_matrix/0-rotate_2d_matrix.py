4a96890854fc056d7cd284e6ea7507a7f4d7b8fb
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-07-18 20:14:35 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,12 +9,10 @@ def rotate_2d_matrix(matrix):
          matrix (list[[list]]): a matrix
     """
     n = len(matrix)
-    
     for i in range(int(n / 2)):
         y = (n - i - 1)
         for j in range(i, y):
             x = (n - 1 - j)
-            
             tmp = matrix[i][j]
             matrix[i][j] = matrix[x][i]
             matrix[x][i] = matrix[y][x]
