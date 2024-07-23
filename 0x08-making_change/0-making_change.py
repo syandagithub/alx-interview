895b3522aa300420fe5a5e85df83e75061f82b6f
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-07-23 16:29:20 +0200

    Create 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Making change function"""
+
+def makeChange(coins, total):
+
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
